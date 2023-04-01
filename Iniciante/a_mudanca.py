37ac431de6300ec5fede5c99e580856d5d8a96ed
Marcos Glasner <marcosglasner@gamil.com>
2023-04-01 13:25:21 +0100

    Adicionando programa a_mudanca.py

---
+++ Iniciante/a_mudanca.py
@@ -0,0 +1,25 @@
+def a_mudanca():
+    """
+    Esta função calcula em que hora do dia se encontra
+    uma pessoa dada os valores do angulo do sol e da lua.
+    O programa deve printar como saída uma saudação conforme
+    cada caso, "Bom Dia!!", "Boa Tarde!!", "Boa Noite!!" e
+    "De Madrugada!!"
+    :return:
+    """
+    while True:
+        try:
+            angulo = int(input())
+            if angulo == 360 or 0 <= angulo < 90:
+                print("Bom Dia!!")
+            elif 90 <= angulo < 180:
+                print("Boa Tarde!!")
+            elif 180 <= angulo < 270:
+                print("Boa Noite!!")
+            elif 270 <= angulo < 360:
+                print("De Madrugada!!")
+        except EOFError:
+            return
+
+
+a_mudanca()
