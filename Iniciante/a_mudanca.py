2c5be5e0d6a75e8b6a5f35ccbb2affa6ee6eb65d
Marcos Glasner <marcosglasner@gamil.com>
2023-04-01 16:03:28 +0100

    Atualizando o Docstrings do programa a_mudanca.py

--- Iniciante/a_mudanca.py
+++ Iniciante/a_mudanca.py
@@ -1,8 +1,8 @@
 def a_mudanca():
     """
     Esta função calcula em que hora do dia se encontra
-    uma pessoa dada os valores do angulo do sol e da lua.
-    O programa deve printar como saída uma saudação conforme
+    uma pessoa, dada os valores do angulo do sol e da lua, e
+    retorna um printe com a saída uma saudação, conforme
     cada caso, "Bom Dia!!", "Boa Tarde!!", "Boa Noite!!" e
     "De Madrugada!!"
     :return:
