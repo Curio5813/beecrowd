7aad82eaafce4f989a5d15264d144e2b9d50c3db
Marcos Glasner <marcosglasner@gamil.com>
2023-04-05 19:52:49 +0100

    Adicionando programa ad_hoc/sequencia_secreta.py

---
+++ ad_hoc/sequencia_secreta.py
@@ -0,0 +1,37 @@
+def sequencia_secreta():
+    """
+    Esta função calcula o número máximo de sequência entre os números
+    1 e 2 sem repetição. A função retorna e printa esse número máximo.
+    :return:
+    """
+    n = int(input())
+    # o valor da variável cont começa em 1 para que o código
+    # abaixo funcione, mas o problema terá pelo todas as
+    # sequências correta com valor maior ou igual a 3.
+    l1, k, cont = [], 0, 1
+    for i in range(n):
+        num = int(input())
+        l1.append(num)
+    b = k + 1
+    while k <= len(l1) - 1:
+        while l1[k] == 1 and l1[b] != 2 or l1[k] == 2 and l1[b] != 1:
+            b += 1
+            if b == len(l1) - 1:
+                if l1[b - 1] == 1 and l1[b] == 2 or l1[b - 1] == 2 and l1[b] == 1:
+                    cont += 1
+                    return print(cont)
+                else:
+                    return print(cont)
+        else:
+            cont += 1
+            k = b
+            if k == len(l1) - 2:
+                if l1[k] == 1 and l1[k + 1] == 2 or l1[k] == 2 and l1[k + 1] == 1:
+                    cont += 1
+                    return print(cont)
+                else:
+                    return print(cont)
+    return print(cont)
+
+
+sequencia_secreta()
