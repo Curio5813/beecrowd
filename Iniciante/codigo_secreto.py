d0934ad66c75dfaf6743aa373035e851d0bd27c4
Curio5813 <marcosglasner@gmail.com>
2023-05-02 16:03:05 +0100

    Adicionando ao repositório o programa codigo_secreto.py

--- Iniciante/codigo_secreto.py
+++ Iniciante/codigo_secreto.py
@@ -1,15 +1,28 @@
 def codigo_secreto():
-    """
-    Esta função retorna a frase contida no codigo secreto
-    formados por pontos e espaços.
-    """
-    alfabeto = "abcdefghijklmnopqrstuvwxyz"
-    l1, l2, dot, cont = [], [], ".", 0
-    for i in alfabeto:
-        l1.append(i)
-    for k in range(len(alfabeto), 3):
-        dot += "."
-    print(dot)
+    while True:
+        try:
+            """
+            Esta função retorna a frase contida no codigo secreto
+            formados por pontos e espaços.
+            """
+            l1 = ['.', '..', '...', '. .', '.. ..', '... ...', '. . .', '.. .. ..', '... ... ...',
+                  '. . . .', '.. .. .. ..', '... ... ... ...', '. . . . .', '.. .. .. .. ..', '... ... ... ... ...',
+                  '. . . . . .', '.. .. .. .. .. ..', '... ... ... ... ... ...', '. . . . . . .', '.. .. .. .. .. .. ..',
+                  '... ... ... ... ... ... ...', '. . . . . . . .', '.. .. .. .. .. .. .. ..',
+                  '... ... ... ... ... ... ... ...', '. . . . . . . . .', '.. .. .. .. .. .. .. .. ..',
+                  '... ... ... ... ... ... ... ... ...']
+            l2, l3 = [], []
+            for j in "abcdefghijklmnopqrstuvwxyz":
+                l2.append(j)
+            n = int(input())
+            for m in range(n):
+                dot = input()
+                h = l1.index(dot)
+                l3.append(l2[h])
+            for o in range(0, len(l3)):
+                print(l3[o])
+        except EOFError:
+            break
 
 
 codigo_secreto()
