3f4ac2f67a772d10ca540b1c3e8df1fd2700ed5a
Curio5813 <marcosglasner@gmail.com>
2023-05-05 00:44:36 +0100

    Adicionando programas no repositório

---
+++ Iniciante/saida_09.py
@@ -0,0 +1,7 @@
+frase1 = "Ro'b'er    to\/"
+frase2 = '("_")'
+frase3 = "('.')"
+print(f'"{frase1}"')
+print("(._.) ( l: ) ( .-. ) ( :l ) (._.)")
+print("(^_-) (-_-) (-_^)")
+print(f"{frase2} {frase3}")
---
+++ ad_hoc/sub_prime.py
@@ -0,0 +1,12 @@
+def sub_prime():
+    """
+    Esta função retorna um caracter "S" ou "N". Se sim o banco
+    será capaz de pagar seu empréstimo, caso contrário será não,
+    o banco não será capaz de pagar seu empréstimo.
+    :return:
+    """
+    dados = input().split(" ")
+    b, n = int(dados[0]), int(dados[1])
+
+
+sub_prime()
