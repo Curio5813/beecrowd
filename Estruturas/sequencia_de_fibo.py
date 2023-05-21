1b808cdec0e2b19709f0777b79fa78f49920dea5
Curio5813 <marcosglasner@gmail.com>
2023-05-21 11:49:43 +0100

    Updating programa.

--- Estruturas/sequencia_de_fibo.py
+++ Estruturas/sequencia_de_fibo.py
@@ -1,33 +1,31 @@
 import decimal
 
+
 def sequencia_fibo():
     """
     Esta função toma a Sequencia de Fibonnaci como parâmetro
     alterada pela seguinte operações aritméticas, a nova
     sequencia começa com fibo(A) e multiplicado fibo(B). Depois
-    faz fibo(A + 1) * fibo(B + 1). O programa termina quando é
-    informado A = 0, B = 0 e N = 0.
+    faz fibo(A + 1) * fibo(B + 1). O programa para quando é informado
+    A = 0, B = 0 e N = 0.
     :return:
     """
     l1 = []
     raiz = decimal.Decimal(5).sqrt()
-    for i in range(0, 100_000_000 + 1):
+    for i in range(0, 10_000 + 1):
         fibo = int(((1 + raiz) ** i - (1 - raiz) ** i) / (2 ** i * raiz))
         l1.append(fibo)
     while True:
-        l2, soma = [], 0
+        l2 = []
         a, b, n = input().split(" ")
         a, b, n = int(a), int(b), int(n)
         if a == 0 and b == 0 and n == 0:
             break
         for i in range(n):
-            if a >= len(l1) - 1 or b >= len(l1) - 1:
-                break
-            n = (l1[a] * l1[b]) % 1000000007
-            soma += n
+            l2.append((l1[a] * l1[b]) % 1000000007)
             a += 1
             b += 1
-        print(soma)
+        print(sum(l2))
 
 
 sequencia_fibo()
