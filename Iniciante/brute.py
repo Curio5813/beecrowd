d2c3d35df22da4d7206d55afe36b875eedfa93ce
Curio5813 <marcosglasner@gmail.com>
2023-07-28 16:37:00 +0100

    Adicionando programa ao repositório.

---
+++ Iniciante/brute.py
@@ -0,0 +1,14 @@
+def brute():
+    """
+    Esta função retorna como valor a quantidade de letras
+    existente no numeral 1000 escrito por extenso em inglês.
+    :return:
+    """
+    x = int(input())
+    for i in range(1, x + 1):
+        for k in range(x, 100 + 1):
+            continue
+    print(4)
+
+
+brute()
