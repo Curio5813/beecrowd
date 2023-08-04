1a567ae45dad1eb47b105a3ed484098cfd095fcd
Curio5813 <marcosglasner@gmail.com>
2023-08-04 15:42:39 +0100

    Adicionando programa calculando.py ao repositório.

--- Iniciante/calculando.py
+++ Iniciante/calculando.py
@@ -4,7 +4,7 @@ def calculando():
     de números dados como parâmetros de entrada.
     :return:
     """
-    m, i, numeros, num, signal, n = '1', 0, [], "", [], 0
+    m, numeros, num, signal, soma, n = '1', [], "", [], 0, 1
     while m != '0':
         m = input()
         if m == '0':
@@ -20,6 +20,41 @@ def calculando():
                 num = ""
             elif x[i] in "0123456789":
                 num += x[i]
+        if len(numeros) == 1:
+            print(f"Teste {n}")
+            print(numeros[0])
+            print("")
+        else:
+            for i in range(0, len(signal)):
+                if i >= len(signal) - 1 and len(signal) < 2:
+                    if signal[i] == '+':
+                        soma = numeros[0] + numeros[1]
+                        break
+                    elif signal[i] == '-':
+                        soma = numeros[0] - numeros[1]
+                        break
+                if i >= len(signal) - 1 and len(signal) >= 2:
+                    if signal[i] == '+':
+                        soma += numeros[i + 1]
+                        break
+                    elif signal[i] == '-':
+                        soma += -numeros[i + 1]
+                        break
+                if i == 0:
+                    if signal[i] == '+':
+                        soma = numeros[0] + numeros[1]
+                    elif signal[i] == '-':
+                        soma = numeros[0] - numeros[1]
+                elif i > 0:
+                    if signal[i] == '+':
+                        soma += numeros[i + 1]
+                    elif signal[i] == '-':
+                        soma += -numeros[i + 1]
+            print(f"Teste {n}")
+            print(soma)
+            print("")
+        n += 1
+        numeros, num, signal, soma = [], "", [], 0
 
 
 calculando()
