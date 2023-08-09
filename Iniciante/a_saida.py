79a4755de7e65d50fbc7720f91000ea74dadb8ca
Curio5813 <marcosglasner@gmail.com>
2023-08-09 18:23:05 +0100

    Adicionanod programa ao repositório.

---
+++ Iniciante/a_saida.py
@@ -0,0 +1,12 @@
+def a_saida():
+    """
+    Esta função tem como retorno, para cada caso teste, a palavra
+    "gzuz".
+    :return:
+    """
+    c = int(input())
+    for i in range(c):
+        print("gzuz")
+
+
+a_saida()
