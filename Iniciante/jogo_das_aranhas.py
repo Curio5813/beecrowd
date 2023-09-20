deba0c69de27fd8efeabd34d79c20fdf1b447dca
Curio5813 <marcosglasner@gmail.com>
2023-09-20 16:32:33 +0100

    Uploading programs.

--- Iniciante/jogo_das_aranhas.py
+++ Iniciante/jogo_das_aranhas.py
@@ -1,4 +1,6 @@
-import math
+from time import time
+
+start = time()
 
 
 def jogo_das_aranhas():
@@ -10,18 +12,37 @@ def jogo_das_aranhas():
     aranhas-vermelhas, dado como entrada um numero inteiro
     que é número de aranhas-marrons e, por conseguinte, de
     aranhas vermelhas, sendo que as aranhas-marrons vêm primereiro
-    no círculo. Merlin tem que dizer um número onde somente as aranhas-marrons
-    são mortas, para assim, vencer o jogo. Um vez morta uma aranhas, deves-se
-    contar somente com as aranhas restantes.
+    no círculo. Merlin tem que dizer um número onde somente as
+    aranhas-marrons são mortas, para assim, vencer o jogo. Um vez
+    morta uma aranhas, deves-se     contar somente com as aranhas
+    restantes.
     :return:
     """
     n = int(input())
-    total = n * 2
-    v = int(total / 2 + 1)
-    prod = 1
-    for i in range(v, total + 1):
-        prod *= i
-    print(prod)
+    aranhas, marrons, vermelhas, aux, k = [], [], [], [], 5
+
+    for i in range(1, n * 2 + 1):
+        aranhas.append(i)
+        aux.append(i)
+        if i <= n:
+            vermelhas.append(i)
+        if i > n:
+            marrons.append(i)
+    for k in range(n + 1, 2_000_000_000 + 1):
+        j = k
+        while True:
+            if len(aranhas) == n:
+                break
+            while k <= len(aranhas):
+                aranhas.pop(k - 1)
+                k += j
+            k = k - len(aranhas) - 1
+        if aranhas == vermelhas:
+            return print(k)
+        elif aranhas != vermelhas:
+            aranhas = aux
 
 
 jogo_das_aranhas()
+end = time()
+print(f"{(end - start) / 60}")
--- teste_02.py
+++ teste_02.py
@@ -1,9 +1,14 @@
-a, n = 998_100, 10
-a //= 1000
-a1 = a
-while a > 10:
-    a %= n
-    print(a)
+aux = [1, 2, 3, 4, 5, 6]
+k = 5
+for i in range(10):
+    if len(aux) == 3:
+        break
+    while k <= len(aux):
+        aux.pop(k - 1)
+        k += 5
+    k = k - len(aux) - 1
+
+print(aux)
 
 
 
