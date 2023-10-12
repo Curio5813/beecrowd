47342629074c0618d4364029fe644d2ab41d51fc
Curio5813 <marcosglasner@gmail.com>
2023-10-12 11:36:26 +0100

    Adicionando programa ao repositório. O programa está em pleno funcionamento.

--- Iniciante/desviando_de_arvores_de_natal.py
+++ Iniciante/desviando_de_arvores_de_natal.py
@@ -15,9 +15,6 @@ def desviando_de_arvores_de_natal():
             obst.append(aux)
         for i in range(0, len(obst)):
             if i == 0:
-                cont += 0
-                pos = "c"
-            elif i == 1:
                 if obst[i][0] == 0 and obst[i][1] == 0 and obst[i][2] == 0:
                     cont += 0
                     pos = "c"
@@ -40,6 +37,68 @@ def desviando_de_arvores_de_natal():
                     elif obst[i + 1] == 1 and obst[i + 1][2] == 0:
                         cont += 1
                         pos = "r"
+            elif i >= 1 and pos == "c":
+                if obst[i][0] == 0 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "c"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "c"
+                elif obst[i][0] == 1 and obst[i][1] == 1 and obst[i][2] == 0:
+                    cont += 1
+                    pos = "r"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 1:
+                    cont += 0
+                    pos = "c"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 1:
+                    cont += 1
+                    pos = "l"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 0 and i < len(obst) - 1:
+                    if obst[i + 1][0] == 0 and obst[i + 1][2] == 1:
+                        cont += 1
+                        pos = "l"
+                    elif obst[i + 1] == 1 and obst[i + 1][2] == 0:
+                        cont += 1
+                        pos = "r"
+            elif i >= 1 and pos == "l":
+                if obst[i][0] == 0 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "l"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 1
+                    pos = "c"
+                elif obst[i][0] == 1 and obst[i][1] == 1 and obst[i][2] == 0:
+                    cont += 2
+                    pos = "r"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 1:
+                    cont += 1
+                    pos = "c"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 1:
+                    cont += 0
+                    pos = "l"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "l"
+            elif i >= 1 and pos == "r":
+                if obst[i][0] == 0 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "r"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "r"
+                elif obst[i][0] == 1 and obst[i][1] == 1 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "r"
+                elif obst[i][0] == 1 and obst[i][1] == 0 and obst[i][2] == 1:
+                    cont += 1
+                    pos = "c"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 1:
+                    cont += 2
+                    pos = "l"
+                elif obst[i][0] == 0 and obst[i][1] == 1 and obst[i][2] == 0:
+                    cont += 0
+                    pos = "r"
+        print(cont)
 
 
 desviando_de_arvores_de_natal()
