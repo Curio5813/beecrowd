0837e2cb2ca43946f7ff58ebe9f6029583270bd5
Curio5813 <marcosglasner@gmail.com>
2023-11-01 11:07:26 +0000

    Atualizando o programa um_problema_facil.py, o programa ainda não está funcional.

--- Matematica/um_problema_facil.py
+++ Matematica/um_problema_facil.py
@@ -1,28 +1,20 @@
-from csv import reader
-
-
 def um_problema_facil():
     """
     Esta função recebe um valor de entrada, calcula e imprime
     a base mínima numérica para o valor dado.
     :return:
     """
-    arq = open("teste01.csv")
-    lista = list(reader(arq, delimiter=" "))
-    l1 = []
-    for i in range(0, len(lista)):
-        for k in range(0, len(lista[i])):
-            l1.append(lista[i][k])
-    n = input()
-    base = []
-    for i in n:
-        for k in range(0, len(l1)):
-            if i in l1[k]:
-                if i == "0" or i == "1":
-                    print(2)
-                else:
-                    base.append(l1.index(l1[k]) + 1)
-    print(max(base))
+    while True:
+        try:
+            bases = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz"
+            n = input()
+            aux, maior, bol = 0, 0, 1
+            for i in range(0, len(n)):
+                aux = bases.index(n[i]) + 1
+                maior = aux
+            print(maior)
+        except EOFError:
+            break
 
 
 um_problema_facil()
