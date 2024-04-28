1b4449ed20532e58967824edda28f06145428d0d
Curio5813 <marcosglasner@gmail.com>
2024-04-28 13:21:50 +0100

    Mudanças no Docstrings do programa.

--- Iniciante/a_arvore_de_natal.py
+++ Iniciante/a_arvore_de_natal.py
@@ -6,8 +6,8 @@ def a_arvore_de_natal():
     e não deve ser maior que 300 cm, a espessura do tronco
     da árvore deve ter 50 cm de diâmetro ou mais, e o numero
     de galhos deve ser igual ou maior que 150. A função printa
-    'Sim', se a arvore de natal está de acordo com as especificações
-    de Roberto ou 'Nao', caso contrário.
+    'Sim', se a arvore de natal está de acordo com as
+    especificações de Roberto ou 'Nao', caso contrário.
     :return:
     """
     n = int(input())
