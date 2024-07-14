d987cac0723712a4ef30a5a7ebb2c8199358c260
Curio5813 <marcosglasner@gmail.com>
2024-07-14 14:37:28 +0100

    Atualizando o programa de teste teste_01.py. O programa é usado para fazer testes sobre problemas de programção.

--- teste_01.py
+++ teste_01.py
@@ -1,9 +1,62 @@
-booleano = 0
-for i in range(1, 100):
-    for k in range(1, 100):
-        if i != k and i ** k == k ** i:
-            print(i, k)
-            booleano = 1
+"""
+Observações sobre alguns problemas.
+"""
+
+
+dados, estrutura, estruturas = [1, 2, 3], [], []
+x1 = [1, 1, 1, 1, 1, 1, 1]  # 1
+x2 = [2, 1, 1, 1, 1, 1]  # 6
+x3 = [2, 2, 1, 1, 1]  # 5
+x4 = [2, 2, 2, 1]  # 4
+x5 = [3, 1, 1, 1, 1] # 5
+x6 = [3, 3, 1]  # 3
+x7 = [3, 1, 1, 2]  # 4
+x8 = [3, 2, 2] # 3
+
+
+n = int(input())
+
+for i in range(n):
+    estrutura.append(dados[0])
+estruturas.append(estrutura)
+temp = estrutura.copy()
+a, b = 0, 1
+cont = 0
+for i in range(n):
+    if not temp:
+        temp = estrutura.copy()
+        a = 0
+        b += 1
+        temp.insert(a, dados[b])
+    else:
+        temp.insert(a, dados[b])
+        temp.pop()
+    while sum(temp) != n:
+        if not temp:
             break
-    if booleano == 1:
-        break
+        temp.pop()
+    if sum(temp) == n:
+        # print(a, temp, end=" ")
+        if a == 0:
+            estruturas.append(temp)
+            temp = estruturas[-1].copy()
+            a += 1
+        else:
+            if dados[b] == 3:
+                estruturas.append(temp)
+                temp = estruturas[a + 1].copy()
+                a += 1
+                b = 1
+                print(dados[b])
+            else:
+                estruturas.append(temp)
+                temp = estruturas[a + 1].copy()
+                a += 1
+        # print(temp, estruturas, i)
+print(estruturas)
+soma = 1
+for i in range(1, len(estruturas)):
+    soma += len(estruturas[i])
+print(soma)
+
+
