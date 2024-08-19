85bc0e4960ceba95182d9ab41ad07d9b3addbf30
Curio5813 <marcosglasner@gmail.com>
2024-08-19 08:51:21 +0100

    Debugando o programa geometria_por_que_nao.py e atualizando o programa teste_01.py

--- Competicoes/geometria_por_que_nao.py
+++ Competicoes/geometria_por_que_nao.py
@@ -1,6 +1,3 @@
-from math import sqrt
-
-
 def geometria_por_que_nao():
     """
     :return:
@@ -54,10 +51,10 @@ def geometria_por_que_nao():
                         area2 = round(altura_menor * base_menor / 2, 3)
                         area += round(area1 + area2, 3)
                         sobras1.append(area2)
-                        # print(area, base, base_menor, reta_h, altura, altura_menor, "Ok0")
+                        print(area, area2, base, base_menor, reta_h, altura, altura_menor, "Ok0")
                         base = round(base_menor, 3)
                         altura = 0
-                    if reta_h <= altura:
+                    elif reta_h <= altura:
                         base = round(base, 3)
                         altura1 = round(base * m, 3)
                         area += altura1 * base / 2
@@ -67,25 +64,26 @@ def geometria_por_que_nao():
                         altura2 = round(base2 * m, 3)
                         altura3 = round(base3 * m, 3)
                         area3 = round(base3 * altura2, 3)
-                        # print(area, area3, base, altura2, altura3, base2, base3, "Ok1")
+                        print(area, area3, base, altura2, altura3, base2, base3, "Ok1")
                         sobras2.append(area3)
                         pontos_y.append(y)
                         altura2 = round(altura - altura1, 3)
                         altura -= altura2
                         altura = round(altura, 3)
-                        # print(area, base, altura, altura1, altura2, x, y, "Ok3")
+                        print(area, base, altura, altura1, altura2, x, y, "Ok2")
                         base = 0
                     if len(pontos_y) >= 2:
                         for n in range(0, len(pontos_y)):
                             if pontos_y.count(pontos_y[n]) >= 2 and pontos_y[n] not in achados:
                                 sobras3.append(sobras2[pontos_y.index(pontos_y[n])])
                                 achados.append(pontos_y[n])
+                print(sobras1, sobras3)
                 if len(sobras1) > 0:
                     area -= sum(sobras1)
                 if len(sobras3) > 0:
                     area -= sum(sobras3)
                 print(f"{k}. {area:.3f}")
-            elif m == 0 or x == 0 or y == 0:
+            else:
                 area = 0
                 print(f"{k}. {area:.3f}")
 
