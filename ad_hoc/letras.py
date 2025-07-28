9aa088fffd00e35d6ff46a7b3bc85d222db6fc6a
Curio5813 <marcosglasner@gmail.com>
2025-07-28 15:39:04 +0100

    Adicionando o programa 'letras.py' ao repositório.

---
+++ ad_hoc/letras.py
@@ -0,0 +1,34 @@
+def letras():
+    """
+    Considere as definições abaixo:
+
+    Uma palavra é uma sequência de letras consecutivas.
+    Um texto é um conjunto de palavras separadas pelo caractere espaço em branco.
+    Você foi contratado pela empresa Booble para escrever um programa que, dados uma
+    letra e um texto, determina a porcentagem de palavras do texto que contém a letra dada.
+
+    Entrada
+    A primeira linha da entrada contém um único caractere, a letra de interesse na pesquisa.
+    A segunda linha contém um texto, como definido acima. O texto é composto apenas por
+    letras minúsculas e o caractere espaço em branco, o texto é formado por no mínimo um
+    caractere, e no máximo 1000 caracteres, o texto não contém dois espaços em branco consecutivos.
+
+    Saída
+    Seu programa deve produzir uma única linha, contendo um único número real, a porcentagem de
+    palavras do texto que contêm a letra dada, com precisão de uma casa decimal.
+    :return:
+    """
+    letra = input()
+    texto = list(input().split(" "))
+    numero_palavras = len(texto)
+    cont = 0
+    for i in range(0, len(texto)):
+        if letra in texto[i]:
+            cont += 1
+    if cont == 0:
+        print(0.0)
+    else:
+        print(f"{(cont/numero_palavras) * 100:.1f}")
+
+
+letras()
