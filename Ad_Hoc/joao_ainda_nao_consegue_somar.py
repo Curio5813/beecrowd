0abd15138ee1a789c1c3414c19afd37866f8879f
Curio5813 <marcosglasner@gmail.com>
2026-09-23 01:42:52 +0100

    O programa está sendo depurado.

--- Ad_Hoc/joao_ainda_nao_consegue_somar.py
+++ Ad_Hoc/joao_ainda_nao_consegue_somar.py
@@ -37,55 +37,25 @@ def joao_ainda_nao_consegue_somar():
             print()
         achados1, achados2, temp2, temp3, cont1, cont2, a = [], [], 0, 0, 1, 1, 1
         tabela2 = deepcopy(tabela)
-        for j in range(1, len(tabela)):
-            flag = False
+        for j in range(1, len(tabela[0])):
             for k in range(1, len(tabela[j])):
-                # print(tabela[j][k])
-                temp2, temp3 = tabela[k][0], tabela[j][0]
-                if achados1 == [] and achados2 == []:
-                    while temp3 < 10000:
-                        print(temp2, temp3)
-                        if tabela[j][k] > 0:
-                            temp3 += 1
-                            if temp3 + temp2 == tabela[j][k]:
-                                flag = True
-                                break
-                        if tabela[j][k] < 0:
-                            temp3 -= 1
-                            if temp3 < -10000:
-                                break
-                            if temp3 + temp2 == tabela[j][k]:
-                                flag = True
-                                break
-                    if achados1 == [] and achados2 == []:
-                        achados1.append(temp3)
-                        achados2.append(temp2)
-                        break
-                elif achados1 != [] and achados2 != [] and k > 1:
-                    temp2, temp3 = tabela2[k][cont1], tabela2[j][cont2]
-                    while temp2 < 10000:
-                        print(temp2, temp3)
-                        if tabela[j][k] > tabela2[j][cont2]:
-                            temp2 += 1
-                            if temp3 + temp2 == tabela[j][k]:
-                                flag = True
-                                achados1.append(temp3)
-                                achados2.append(temp2)
-                                break
-                        elif tabela[j][k] < tabela2[j][cont2]:
-                            temp2 -= 1
-                            if temp2 < -10000:
-                                break
-                            if temp3 + temp2 == tabela[j][k]:
-                                flag = True
-                                achados1.append(temp3)
-                                achados2.append(temp2)
-                                break
-                        cont1 += 1
-                        if cont1 > len(tabela2) - 1:
-                            cont1 = 0
-                if flag == True:
-                    break
+                temp2, temp3 = tabela[j][0], tabela[k][0]
+                while temp3 < 10000:
+                    print(temp2, temp3)
+                    if tabela[j][k] > 0:
+                        temp3 += 1
+                        if temp3 + temp2 == tabela[j][k]:
+                            flag = True
+                            break
+                    if tabela[j][k] < 0:
+                        temp3 -= 1
+                        if temp3 < -10000:
+                            break
+                        if temp3 + temp2 == tabela[j][k]:
+                            flag = True
+                            break
+                tabela2[j][0] = temp3
+                break
         if flag:
             print(f"{caso}. YES")
             caso += 1
@@ -96,7 +66,7 @@ def joao_ainda_nao_consegue_somar():
             for k in range(len(tabela2[j])):
                 print(f"{tabela2[j][k]:>5}", end=' ')
             print()
-        print(achados1, achados2)
+        print(achados1)
 
 
 joao_ainda_nao_consegue_somar()
